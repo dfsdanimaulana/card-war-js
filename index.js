e02b861a2bc6daa66a9c732c19307a3683c82809
dfsdanimaulana <danimaulana9f@gmail.com>
2022-03-05 22:13:17 +0700

    move deck.js to index.js

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-import Deck from './Deck.js'
+const SUITS = ["♠", "♣", "♥", "♦"]
+const VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+]
 
 const CARD_VALUE_MAP = {
   "2": 2,
@@ -23,6 +38,61 @@ const playerDeckElement = $('.player-deck')
 const text = $('.text')
 
 let playerDeck, computerDeck, inRound, stop
+
+class Deck {
+  constructor(cards = freshDeck()){
+    this.cards = cards
+  }
+  
+  get numberOfCards() {
+    return this.cards.length
+  }
+  
+  pop(){
+    return this.cards.shift()
+  }
+  
+  push(card){
+    return this.cards.push(card)
+  }
+  
+  shuffle(){
+    for (let i = this.numberOfCards - 1; i > 0; i--) {
+      const newIndex = Math.floor(Math.random() * (i + 1))
+      const oldIndex = this.cards[newIndex]
+      this.cards[newIndex] = this.cards[i]
+      this.cards[i] = oldIndex
+    }
+  }
+}
+
+class Card {
+  constructor(suit, value){
+    this.suit = suit
+    this.value = value
+  }
+  
+  get color(){
+    return this.suit === "♠" || this.suit === "♣" ? 'black' : 'red'
+  }
+  getHTML(){
+    const cardDiv = document.createElement('div')
+    cardDiv.innerText = this.suit
+    cardDiv.classList.add('card', this.color)
+    cardDiv.dataset.value = `${this.value} ${this.suit}`
+    return cardDiv
+  }
+}
+
+function freshDeck() {
+  return SUITS.flatMap(suit => {
+    return VALUES.map(value => {
+      return new Card(suit,value)
+    })
+  })
+}
+
+// tutorial by https://youtu.be/NxRwIZWjLtE
 
 document.addEventListener('click', ()=>{
   if(stop){
